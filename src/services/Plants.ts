c372a35748ff304493d4fa5d5f156fc07bf7d8d3
Kami Takegami <99816952+KMTB00052571903@users.noreply.github.com>
2025-05-09 18:14:45 -0500

    Create commits and adding plant interface

--- src/services/Plants.ts
+++ src/services/Plants.ts
@@ -1,4 +1,25 @@
 // TODO: arregla typos de "error", maneja caso de error y exito
-export async function getPlants(): Promise<any> {
-    return [];
+export interface Plant {
+    id: number;
+    commonName: string;
+    scientificName: string;
+    image: string;
+    description: string;
+    careInstructions: string;
+    sunLight: string;
+    water:string;
+    temperature:string;
+}
+export async function getPlants(): Promise<Plant[]> {
+    try {
+        const response = await fetch('http://192.168.131.101:8080/dca/api/plants');
+        if (!response.ok) {
+            throw new Error('Error al obtener las plantas');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 }
